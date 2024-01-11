b3a047706d711b076ce250279a89fd504d5cca89
Guyoguyes <g.abduba43@gmail.com>
2024-01-11 14:21:45 +0300

    email configuration

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,6 +1,43 @@
+"use client"
+import React, { useState } from "react";
 import NewsLatterBox from "./NewsLatterBox";
+import { Resend } from "resend";
+
+const resend = new Resend('re_RV2vYxxe_P3kfgSQAiHXeMzbZ853mU73o');
+
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    
+    try {
+        if (name === "" || email === "" || message === "" || subject === "") { 
+            alert('Please fill in all the inputs');
+        } else {
+            resend.emails.send({
+                from: email,
+                to: 'aguyo@mhealthkenya.org',
+                subject: subject,
+                html: `<p>Congrats on sending your <strong>${name}</strong>!</p>`
+            });
+            alert('Email sent successfully!');
+        }
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        alert('Email Not sent Error!!!');
+    }
+}
+
+
+  
+
   return (
     <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
       <div className="container">
@@ -17,7 +54,7 @@ const Contact = () => {
               <p className="mb-12 text-base font-medium text-body-color">
                 Our team will get back to you ASAP via email.
               </p>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="-mx-4 flex flex-wrap">
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
@@ -31,6 +68,8 @@ const Contact = () => {
                         type="text"
                         placeholder="Enter your name"
                         className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
+                        value={name} onChange={(e) => setName(e.target.value
+                          )}
                       />
                     </div>
                   </div>
@@ -46,6 +85,25 @@ const Contact = () => {
                         type="email"
                         placeholder="Enter your email"
                         className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
+                        value={email} onChange={(e) => setEmail(e.target.value
+                          )}
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="email"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Topic
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="Enter your subject"
+                        className="w-full rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
+                        value={subject} onChange={(e) => setSubject(e.target.value
+                          )}
                       />
                     </div>
                   </div>
@@ -62,11 +120,13 @@ const Contact = () => {
                         rows={5}
                         placeholder="Enter your Message"
                         className="w-full resize-none rounded-md border border-transparent py-3 px-6 text-base text-body-color placeholder-body-color shadow-one outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#242B51] dark:shadow-signUp"
+                        value={message} onChange={(e) => setMessage(e.target.value
+                          )}
                       ></textarea>
                     </div>
                   </div>
                   <div className="w-full px-4">
-                    <button className="rounded-md bg-primary py-4 px-9 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
+                    <button type="submit" className="rounded-md bg-primary py-4 px-9 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
                       Submit 
                     </button>
                   </div>
